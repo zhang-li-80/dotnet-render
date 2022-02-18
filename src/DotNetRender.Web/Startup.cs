972edbe8aa6a8218848737d5dc0c10c87f299c4b
Zhang Li <99843897+zhang-li-80@users.noreply.github.com>
2022-02-18 19:45:35 +0000

    Remove https for deploy.

--- src/DotNetRender.Web/Startup.cs
+++ src/DotNetRender.Web/Startup.cs
@@ -36,12 +36,10 @@ namespace WebApplication1
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseForwardedHeaders();
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
