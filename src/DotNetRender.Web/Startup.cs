713686bd3e54ca03dde8d6674b45861c93ac1cb2
Zhang Li <99843897+zhang-li-80@users.noreply.github.com>
2022-02-18 19:37:10 +0000

    Add more headers for deploy.

--- src/DotNetRender.Web/Startup.cs
+++ src/DotNetRender.Web/Startup.cs
@@ -36,8 +36,12 @@ namespace WebApplication1
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
+            app.UseForwardedHeaders();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
